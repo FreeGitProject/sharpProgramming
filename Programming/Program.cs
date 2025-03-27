738ac71e73065de728fe89afbb0136fdc6cfd709
Avinash.Gupta <avinash.gupta@bettercommerce.io>
2025-03-27 16:55:11 +0530

    Initial commit

---
+++ Programming/Program.cs
@@ -0,0 +1,17 @@
+﻿using Programming;
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+            int k = 3;
+
+            RotateArray.RotateArrayMethod(nums,k);
+        }
+    }
+}
---
+++ Programming/RotateArray.cs
@@ -0,0 +1,35 @@
+﻿namespace Programming
+{
+    public static  class RotateArray
+    {
+        public static void RotateArrayMethod(int[] nums,int k)
+        {
+
+            for (int i = 0; i < 7; i++)
+                Console.Write(" " + nums[i]);
+
+            int len = nums.Length;
+            int temp = 0;
+            while (k != 0)
+            {
+                int length = nums.Length - 1;
+
+                temp = nums[length];
+                for (int i = 0; i < len; i++)
+                {
+                    if (length == 0)
+                        break;
+                    nums[length] = nums[length - 1];
+                    length--;
+
+                }
+                nums[0] = temp;
+                Console.WriteLine();
+                for (int i = 0; i < len; i++)
+                    Console.Write(" " + nums[i]);
+
+                k--;
+            }
+        }
+    }
+}
