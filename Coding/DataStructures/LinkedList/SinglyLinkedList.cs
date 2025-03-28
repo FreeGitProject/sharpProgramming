8bbfd0626b7e86c77f1a6c96d8f4c6ca9947cecb
Avinash.Gupta <avinash.gupta@bettercommerce.io>
2025-03-28 12:48:33 +0530

    add new

---
+++ Coding/DataStructures/LinkedList/SinglyLinkedList.cs
@@ -0,0 +1,47 @@
+﻿namespace DataStructures.LinkedList;
+
+public class Node<T>
+{
+    public T Data { get; set; }
+    public Node<T>? Next { get; set; }
+
+    public Node(T data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
+
+public class SinglyLinkedList<T>
+{
+    private Node<T>? head;
+
+    public void Add(T data)
+    {
+        var newNode = new Node<T>(data);
+        if (head == null)
+        {
+            head = newNode;
+        }
+        else
+        {
+            var current = head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
+    }
+
+    public void Display()
+    {
+        var current = head;
+        while (current != null)
+        {
+            Console.Write($"{current.Data} -> ");
+            current = current.Next;
+        }
+        Console.WriteLine("null");
+    }
+}
---
+++ Coding/Sandbox/Program.cs
@@ -0,0 +1,9 @@
+﻿using DataStructures.LinkedList;
+
+var list = new SinglyLinkedList<int>();
+list.Add(10);
+list.Add(20);
+list.Add(30);
+
+Console.WriteLine("Singly Linked List:");
+list.Display();
