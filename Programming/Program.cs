55050698624f60c4177bc930b06c2e0f9f4c8370
Avinash.Gupta <avinash.gupta@bettercommerce.io>
2025-04-06 17:06:11 +0530

    updat encapsulation

--- Programming/Program.cs
+++ Programming/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Programming;
+using Programming.Encapsulation;
 
 namespace HelloWorld
 {
@@ -6,14 +7,27 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           // Console.WriteLine("Hello World!");
             int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
             int k = 3;
 
             // RotateArray.RotateArrayMethod(nums,k);
             //   Solution.PrintArray();
             //Solution.TwoSum();
-            Solution.TwoSumuseDisnary();
+            //Solution.TwoSumuseDisnary();
+           // Students obj = new Students();
+
+            //obj.Name = "";
+            //obj.Age = -5;
+            //obj.SetName("");
+            //obj.GetName();
+            //obj.SetAge(-225);
+            //obj.GetAge();
+
+            BankAccount obj = new BankAccount();
+            Console.WriteLine("Intial Balance : " + obj.GetBalance());
+            obj.Deposit(23);
+            Console.WriteLine(obj.GetBalance());
         }
     }
 }
---
+++ Programming/Students.cs
@@ -0,0 +1,52 @@
+﻿using System.Xml.Linq;
+
+namespace Programming
+{
+    internal class Students
+    {
+        private string Name;
+        private int Age;
+
+        public void GetName()
+        {
+            if (string.IsNullOrEmpty(this.Name))
+            {
+
+            }
+            else
+                Console.WriteLine("Name Is : " + this.Name);   
+
+        }
+        public void SetName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Name Is empty ");
+            }
+            else
+                this.Name = name;
+
+        }
+        public void GetAge()
+        {
+            if (Age <= 0)
+            { }
+            else
+
+                Console.WriteLine("Age Is : " + this.Age);
+            
+                
+
+        }
+        public void SetAge(int age)
+        {
+            if (age <= 0)
+                Console.WriteLine("Age is invalid");
+            else
+                this.Age = age;
+
+        }
+
+
+    }
+}
