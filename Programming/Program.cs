9cedf3f584a5cec4707d56c674aca34396b31041
AvinashGit <159417412+FreeGitProject@users.noreply.github.com>
2025-08-19 16:25:50 +0530

    test

--- Programming/Program.cs
+++ Programming/Program.cs
@@ -2,18 +2,108 @@
 using Programming.Abstract;
 using Programming.Dipendency_Injecttion;
 using Programming.Encapsulation;
+using System.Collections;
+using System.Runtime.InteropServices;
 
 namespace HelloWorld
 {
-  
-   
-    class Program
+    interface IEmplyee
+    {
+    public string id { get; set; }
+        public string name { get; set; }
+      //  private dateOfBirth;
+   // private city;
+string FullName(string name + "city");
+    }
+    public  class A
+    {
+
+        public  void Add()
+        {
+
+            Console.WriteLine("Hello A");
+        }
+        //public abstract void Add();
+
+    }
+    public class B : A
+    {
+        public virtual void Add()
+        {
+            Console.WriteLine("Hello B");
+        }
+
+    }
+    public class C : B
+    {
+
+       // private static C _c = new C();
+        // static C()
+        //{
+        //    Console.WriteLine("c ka static constructure");
+        //}
+        //private C()
+        //{
+        //        Console.WriteLine("Defaut constru");
+        //}
+        //public C( string g)
+        //{
+        //    Console.WriteLine(  "parameter");
+        //}
+        //public static C CreateInstance()
+        //{
+        //    return _c;
+        //}
+        public override void Add()
+        {
+            base.Add();
+            Console.WriteLine("Hello C");
+        }
+        public int GetCustomerInfo(string customerId)
+
+        {
+            return 1;
+            //Method implementation
+
+        }
+
+        public string GetCustomerInfo(string customerId,int no=0)
+        {
+            return "d";//string.Empty;
+            //Method implementation 
+        } 
+    }
+        class Program
     {
         static void Main(string[] args)
         {
+         //   int a = 8;
+           // B b1 = new A();
+            A objA = new B();
+            B objB = new B();
+            C objectC = new C();
+            // A objC = new C();
+            //   C objC = new C();
+            // B objB1 = new C();
+            //  A a1 = new A();
+            objA.Add();//a
+            objB.Add();//b
+          //  objC.Add();//b
+           // objB1.Add();//c
+            ArrayList aa = new ArrayList();
+            aa.Add("dd");
+
+           
+            objectC.Add();
+
+            //Console.Write(objC.GetCustomerInfo("23"));
+            //Console.Write(objC.GetCustomerInfo("23",1));
+
+            //var h = C.GetCustomerInfoh("jhf");
+            //a1.Add();   //a
             // Console.WriteLine("Hello World!");
-            int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
-            int k = 3;
+            //int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+            //int k = 3;
 
             // RotateArray.RotateArrayMethod(nums,k);
             //   Solution.PrintArray();
@@ -36,8 +126,8 @@ namespace HelloWorld
 
 
 
-            Account a = new Account();  
-            a.PrintDetail();
+            // Account a = new Account();  
+            // a.PrintDetail();
             //CalulatorAbstract calulatorAbstract = new CalulatorAbstract();
 
         }
---
+++ Programming/SingleTon/SingleTon.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.SingleTon
+{
+    internal class SingleTon
+    {
+        private static SingleTon Instance;
+
+        private SingleTon()
+        {
+            
+        }
+        public static  SingleTon GetInstance()
+        { 
+            if(Instance is null)
+            {
+                Instance = new SingleTon();
+            }
+            return Instance;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("One Instance");
+        }
+    }
+
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            SingleTon singleTon = SingleTon.GetInstance();
+            singleTon.Print();
+        }
+    }
+}
